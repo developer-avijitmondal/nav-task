0534837909557ee57549be46f8c91cf1517b0279
developer-avijitmondal <developer.avijitmondal@gmail.com>
2020-07-13 18:29:35 +0530

    devs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BlogComponent } from './blog/blog.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { AdminlogoutComponent } from './adminlogout/adminlogout.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AdmindashboardComponent } from './admindashboard/admindashboard.compone
     BlogComponent,
     NavbarComponent,
     AdminLoginComponent,
-    AdmindashboardComponent
+    AdmindashboardComponent,
+    AdminlogoutComponent
   ],
   imports: [
     BrowserModule,
