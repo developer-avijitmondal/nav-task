3c3238b6346c5e58194d13525dd1f57b417880e9
developer-avijitmondal <developer.avijitmondal@gmail.com>
2020-07-13 18:46:41 +0530

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { BlogComponent } from './blog/blog.component';
 
 
 const routes: Routes = [
   { path:'home', redirectTo:'',pathMatch:'full' },
   { path:'',component: HomeComponent },
+  { path : 'blog', component : BlogComponent },
   { path : 'admin-login', component : AdminLoginComponent }
 
 ];
